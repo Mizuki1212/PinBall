c95bf90a4e523e0a0fa89eff67751796d7c83409
takafumisasaki <taketakakun22@yahoo.co.jp>
2019-09-22 16:38:56 +0900

    Lesson5 PinBallのコミット
    Android向けにビルド設定しPinBallアプリを作成しました。
    UIで得点テキストの作成と、Score C#スクリプトを新たに作成し、得点を加算出来るようにしました。

---
+++ Assets/Score.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour {
+
+
+	// 課題 UIのテキストを使って得点を表示してください------------------------------------
+
+	// スコアテキストを表示するテキスト
+	private GameObject ScoreText;
+
+	// スコアを0で初期化
+	int score= 0;
+
+	// Use this for initialization
+	void Start () {
+
+		// シーン中にScoreTextオブジェクトを取得
+		ScoreText = GameObject.Find("ScoreText");
+
+		// ScoreTextに0で初期化したスコア変数を表示する
+		ScoreText.GetComponent<Text> ().text = "Score: " + score;
+			
+	}
+
+	 // 「OnCollisionEnter」BallのColliderが他のオブジェクトのColliderと衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision other){
+
+		// 各タグに加算する点数を設定する
+		if(other.gameObject.tag == "SmallStarTag"){
+			score += 10;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			score += 50;
+		} else if (other.gameObject.tag == "LargeStarTag" || other.gameObject.tag == "LargeCloudTag") {
+			score += 100;
+		}
+
+		// ScoreTextに衝突により加算された合計スコア変数を表示する
+		ScoreText.GetComponent<Text> ().text = "Score: " + score;
+
+
+	}
+}
+
+
+
+
---
+++ Assets/StarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour {
+
+	// 回転速度
+	private float rotSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		// 回転を開始する角度を設定(宿題）
+		// 答え：Y軸回転のStarが回転を開始した際に、初期角度がランダムな位置で回転を開始する。回転角度（最小0°～最大360°）
+		this.transform.Rotate (0, Random.Range (0, 360), 0);
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 回転
+		this.transform.Rotate (0, this.rotSpeed, 0);
+		
+	}
+}
